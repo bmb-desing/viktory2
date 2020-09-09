ed168bac43ce2a04e1b0562ed3f543ea7b384fab
bmb-design92 <35960774+xolms111@users.noreply.github.com>
2020-09-10 02:49:00 +0300

    123

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,39 @@
 $(document).ready(() => {
     $(`input[name=phone]`).inputmask(`+7 (999) 999-99-99`)
-    const firstScreenSlider = $(`.first-screen`).owlCarousel({
+    const firstScreenSlider = $(`.first-screen__slider`).owlCarousel({
         items: 1,
         dots: false,
         loop: true,
         autoplay: true,
         autoplayHoverPause: true,
     })
+    $(`.lazy`).lazy()
     firstScreenSlider.on(`changed.owl.carousel`, function (property) {
-        const current = property.item.index - 1
+        const current = (property.property.value - Math.ceil(property.item.count / 2)) % property.item.count || 0
         const count = property.item.count
-        console.log(current, count)
+        console.log(current)
+        changeCurrentFirstScreenSlider(current, count)
     })
+    $(`.first-screen__nav_next`).click(function () {
+        firstScreenSlider.trigger(`next.owl.carousel`)
+    })
+    $(`.first-screen__nav_prev`).click(function () {
+        firstScreenSlider.trigger(`prev.owl.carousel`)
+    })
+    $(window).on(`load`, function () {
+        $(`.loader`).delay(1000).fadeOut()
+    })
+
+    // eslint-disable-next-line no-undef
+    Waves.init()
+    // eslint-disable-next-line no-undef
+    Waves.attach(`.button`, `waves-light`)
 })
+function changeCurrentFirstScreenSlider(current, count) {
+    const persent = ((current + 1) / count * 100)
+    $(`.first-screen__load span`).css(`width`, persent + `%`)
+    if (current + 1 < 10) {
+        current = `0` + (current + 1)
+    }
+    $(`.first-screen__text_current`).text(current)
+}
